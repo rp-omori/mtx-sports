6ce4e5deeae9dcccb8517c96ee99ecfbb67f8dc4
k.omori <k.omori@roseaupensant.jp>
2022-07-12 15:47:45 +0900

    feat: create a header

---
+++ header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, minimum-scale=1, maximum-scale=1, initial-scale=1.0, user-scalable=no">
+    <meta name="description" content="【ページの説明文】" />
+    <title>MTX膝関節クリニック</title>
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/style.css">
+    <link rel="shortcut icon" href="【ファイル名.ico】" />
+    <link rel="apple-touch-icon-precomposed" href="【画像のパス/apple-touch-icon-precomposed.png】" />
+    <!-- facebook -->
+    <meta property="og:url" content="【ページのURL】 /">
+    <meta property="og:title" content="【ページのタイトル】" />
+    <meta property="og:type" content="website" />
+    <meta property="og:description" content="【ページの説明】" />
+    <meta property="og:image" content="【サムネイル画像のURL】" />
+    <meta property="og:site_name" content="【サイトのタイトル】" />
+    <meta property="fb:app_id" content="【appID】" />
+    <meta property="og:locale" content="ja_JP" />
+    <!-- Twitter -->
+    <meta name="twitter:card" content="【カードの種類】" />
+    <meta name="twitter:site" content="【@ユーザー名】" />
+    <meta name="twitter:description" content="【ページの説明】" />
+    <meta name="twitter:image:src" content="【サムネイル画像のURL】" />
+    <?php wp_head();?>
+</head>
+<body>
+    <div class="container">
+        <header>header</header>
