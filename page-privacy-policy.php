2260fd6c2803332b67e97243ee6c83419314b3d3
小池　建 <t.koike@roseaupensant.jp>
2022-08-08 16:16:49 +0900

    #27/feat  プライバシーポリシーページ完成

--- page-privacy-policy.php
+++ page-privacy-policy.php
@@ -14,6 +14,126 @@ $rootDir = get_template_directory_uri();
   <!-- 共通タイトル部分 -->
   <?php get_template_part('component/c__title'); ?>
 
+  <!-- コンテンツ部分 -->
+  <div id="privacy-policy" class="privacy-policy-contents">
+    <div class="contents-box">
+      <div class="contents-box__title">
+        <p>1.個人情報の収集について</p>
+      </div>
+      <div class="contents-box__text">
+        <p>当院は、診療・看護及び患者さんの医療に関わる範囲で個人情報の収集を行います。その他の目的に個人情報を利用する場合は、利用目的をあらかじめお知らせし、ご了解を得た上で実施いたします。</p>
+      </div>
+    </div>
+    <div class="contents-box">
+      <div class="contents-box__title">
+        <p>2.個人情報の利用及び提供について</p>
+      </div>
+      <div class="contents-box__text">
+        <p>
+          当院は、患者さんの個人情報の利用につきましては、以下の場合を除き、本来の利用目的の範囲を超えて使用いたしません。<br>
+          　1. 患者さんの了解を得た場合<br>
+          　2. 特定の個人を識別できない状態に加工して利用する場合<br>
+          　3. 法令等により提供を要求された場合<br>
+          当院は、法令の定める場合等を除き、患者さんの許可なくその情報を第三者に提供いたしません。
+        </p>
+      </div>
+    </div>
+    <div class="contents-box">
+      <div class="contents-box__title">
+        <p>3. 個人情報の適正管理について</p>
+      </div>
+      <div class="contents-box__text">
+        <p>当院は、患者さんの個人情報について、正確かつ最新の状態に保ち、患者さんの個人情報の漏えい、紛失、破壊、改ざん又は患者さんの個人情報への不正なアクセスを防止することに努めます。</p>
+      </div>
+    </div>
+    <div class="contents-box">
+      <div class="contents-box__title">
+        <p>4. 個人情報の開示・修正等について</p>
+      </div>
+      <div class="contents-box__text">
+        <p>当院は、患者さんの個人情報について患者さんが開示を求められた場合には、遅滞なく内容を確認し対応いたします。また内容が事実でない等の理由で訂正を求められた場合も、調査し適切に対応いたします。</p>
+      </div>
+    </div>
+    <div class="contents-box">
+      <div class="contents-box__title">
+        <p>5. 問い合わせの窓口</p>
+      </div>
+      <div class="contents-box__text">
+        <p>当院の個人情報保護基本方針に関するお問い合わせは、【担当部署名および連絡先をご記入ください】でお受けいたします。</p>
+      </div>
+    </div>
+    <div class="contents-box">
+      <div class="contents-box__title">
+        <p>6. 法令遵守と個人情報保護の仕組みの改善</p>
+      </div>
+      <div class="contents-box__text">
+        <p>当院は、個人情報の保護に関する日本の法令、その他の規範を遵守するとともに、上記の各項目の見直しを適宜行い、個人情報保護の仕組みの継続的な改善を図ります。</p>
+      </div>
+    </div>
+  </div>
+
+  <div id="privacy-policy" class="privacy-policy-contents mb200">
+    <div class="contents-box lead-box">
+      <div class="contents-box__title">
+        <p>個人情報収集の目的と利用の範囲</p>
+      </div>
+      <div class="contents-box__text">
+        <p>当院において収集した患者さんの個人情報の利用の目的と利用の範囲は以下の通りです。</p>
+      </div>
+    </div>
+    <div class="contents-box">
+      <div class="contents-box__title">
+        <p>1. 院内での利用</p>
+      </div>
+      <div class="contents-box__text">
+        <p>
+          1. 患者さんに提供する医療サービス<br>
+          2. 医療保険事務<br>
+          3. 入退院等の病棟管理<br>
+          4. 会計・経理<br>
+          5. 医療事故等の報告<br>
+          6. 患者さんへの医療サービスの向上<br>
+          7. 患者さんのお呼び出し<br>
+          8. その他、患者さんに係る管理運営業務
+        </p>
+      </div>
+    </div>
+    <div class="contents-box">
+      <div class="contents-box__title">
+        <p>2. 院外への情報提供としての利用</p>
+      </div>
+      <div class="contents-box__text">
+        <p>
+          1. 他の病院、診療所、助産院、薬局等との連携<br>
+          2. 他の医療機関等からの照会への回答<br>
+          3. 患者さんの診療等のため、外部の医師等の意見・助言を求める場合<br>
+          4. 検体検査業務等の業務委託<br>
+          5. 医療の質の向上を目的とした臨床症例研究業務の委託<br>
+          6. ご家族等への病状説明<br>
+          7. 審査支払機関へのレセプトの提供<br>
+          8. 審査支払機関又は保険者からの照会への回答<br>
+          9. 事業者等から委託を受けた健康診断に係る、事業者等へのその結果通知<br>
+          10. 医師賠償責任保険等に係る、医療に関する専門の団体や保険会社等への相談又は届出等<br>
+          11. 医療事故等の報告<br>
+          12. その他、患者さんへの医療保険事務に関する利用
+        </p>
+      </div>
+    </div>
+    <div class="contents-box">
+      <div class="contents-box__title">
+        <p>3. その他の利用</p>
+      </div>
+      <div class="contents-box__text">
+        <p>
+          1. 医療サービスや業務の維持・改善の為の基礎資料<br>
+          2. 外部監査機関への情報提供<br>
+          ※上記のうち、同意しがたい事項がある場合には、その旨を担当窓口にお申し出下さい。<br>
+          ※お申し出がないものについては、同意していただけたものとして取り扱わせていただきます。
+        </p>
+      </div>
+    </div>
+  </div>
+
 </main>
 
 <?php get_footer(); ?>
