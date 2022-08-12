c6e44e8a02bc0979358c1644f5d89f84dc003287
小池　建 <t.koike@roseaupensant.jp>
2022-08-12 17:56:46 +0900

    #31/feat: priceページ完成+footerの共通パーツ化(footer__other)

---
+++ component/footer__other.php
@@ -0,0 +1,25 @@
+<!-- footer手前のflex部分 -->
+<?php
+$rootDir = get_template_directory_uri();
+?>
+
+<div class="footer__other">
+  <a href="" class="footer__other_box">
+    <div class="footer__other_box_img">
+      <img src="<?php echo $rootDir ?>/images/footer__other/footer_other_01.jpg" alt="">
+    </div>
+    <div class="footer__other_box_text">
+      <p class="orange">MEDICAL SUBJECTS</p>
+      <p class="white">診療科目</p>
+    </div>
+  </a>
+  <a href="" class="footer__other_box">
+    <div class="footer__other_box_img">
+      <img src="<?php echo $rootDir ?>/images/footer__other/footer_other_02.jpg" alt="">
+    </div>
+    <div class="footer__other_box_text">
+      <p class="orange">STAFF</p>
+      <p class="white">医師・スタッフ紹介</p>
+    </div>
+  </a>
+</div>
