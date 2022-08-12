71adccbbd0ecb4d914b99839f6548c60141596af
小池　建 <t.koike@roseaupensant.jp>
2022-08-12 13:06:26 +0900

    #31/feat:共通タイトル箇所の修正(sp時の中央よせ)

---
+++ page-price.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Template Name: privacy-policy
+ */
+
+get_header(); ?>
+<?php
+$rootDir = get_template_directory_uri();
+?>
+
+
+<main class="main">
+
+  <!-- 共通タイトル部分 -->
+  <?php get_template_part('component/c__title'); ?>
+
+  <!-- コンテンツ部分 -->
+  <div class="price-contents"></div>
+
+</main>
+
+<?php get_footer(); ?>
