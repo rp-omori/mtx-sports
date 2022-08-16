59ad2c131f19eb2841e49c880fdc1c5f030d9cc5
小池　建 <t.koike@roseaupensant.jp>
2022-08-16 12:20:19 +0900

    #33/feat:パンくずリストを別バージョンで実装

---
+++ page-medical_detail.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Template Name: privacy-policy
+ */
+
+get_header(); ?>
+<?php
+$rootDir = get_template_directory_uri();
+?>
+
+
+<main class="main">
+
+  <!-- 共通タイトル部分 -->
+  <?php get_template_part('component/c__bread'); ?>
+
+  <!-- コンテンツ部分 -->
+  <div id="medical-detail" class="medical-detail-contents">
+  </div>
+
+</main>
+
+<?php get_footer(); ?>
