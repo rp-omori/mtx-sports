7c6fca9e66e8a307a44e829a3f37fdd80ad6f0d8
小池　建 <t.koike@roseaupensant.jp>
2022-08-25 14:42:06 +0900

    #31/feat: CMS化（途中）

--- page-price.php
+++ page-price.php
@@ -16,38 +16,39 @@ $rootDir = get_template_directory_uri();
 
   <!-- コンテンツ部分 -->
   <div class="price-contents">
+
     <div class="contents-box">
       <div class="contents-box_cat">
         <p>診療</p>
       </div>
-      <div class="contents-box_menu">
-        <div class="menu_inner">
-          <div class="menu_inner_name">
-            <p>初診</p>
-          </div>
-          <div class="menu_inner_minute">
-            <p>20分</p>
-          </div>
-          <div class="menu_inner_price">
-            <p>¥5,000</p>
-          </div>
-        </div>
-        <div class="menu_inner">
-          <div class="menu_inner_name">
-            <p>2回目以降</p>
-          </div>
-          <div class="menu_inner_minute">
-            <p>10分</p>
-          </div>
-          <div class="menu_inner_price">
-            <p>¥3,000</p>
-          </div>
-        </div>
-      </div>
+      <table class="contents-box_menu">
+        <tbody class="contents-box_menu__inner">
+          <tr class="contents-box_menu__inner_box">
+            <td class="contents-box_menu__inner_box_name">
+              <div class="flex">
+                <div class="flex_box">
+                  <p>初診</p>
+                </div>
+              </div>
+            </td>
+            <td class="contents-box_menu__inner_box_minute"><p>20分</p></td>
+            <td class="contents-box_menu__inner_box_price"><p>¥5,000</p></td>
+          </tr>
+          <tr class="contents-box_menu__inner_box">
+            <td class="contents-box_menu__inner_box_name">
+              <div class="flex">
+                <div class="flex_box">
+                  <p>2回目以降</p>
+                </div>
+              </div>
+            </td>
+            <td class="contents-box_menu__inner_box_minute"><p>10分</p></td>
+            <td class="contents-box_menu__inner_box_price"><p>¥3,000</p></td>
+          </tr>
+        </tbody>
+      </table>
       <div class="contents-box_caution">
-        <a href="" class="contents-box_caution_inner">
-          <p>※治療を行なった場合は、診察料を頂戴しません。</p>
-        </a>
+        <p>※治療を行なった場合は、診察料を頂戴しません。</p>
       </div>
     </div>
 
@@ -55,6 +56,273 @@ $rootDir = get_template_directory_uri();
       <div class="contents-box_cat">
         <p>再生医療</p>
       </div>
+      <table class="contents-box_menu">
+        <tbody class="contents-box_menu__inner">
+          <tr class="contents-box_menu__inner_box">
+            <td class="contents-box_menu__inner_box_name width" rowspan="2">
+              <div class="flex">
+                <div class="flex_box">
+                  <p>PRP治療</p>
+                </div>
+                <p class="detail">肩・手・肘・股関節・膝・足関節・その他ブロック</p>
+              </div>
+            </td>
+            <td class="contents-box_menu__inner_box_part" rowspan="1">
+              <p>1部位</p>
+            </td>
+            <td class="contents-box_menu__inner_box_minute">
+              <p>1回</p>
+            </td>
+            <td class="contents-box_menu__inner_box_price">
+              <p>¥150,000</p>
+            </td>
+          </tr>
+          <tr class="contents-box_menu__inner_box">
+            <td class="contents-box_menu__inner_box_part" rowspan="1">
+              <p>2部位</p>
+            </td>
+            <td class="contents-box_menu__inner_box_minute">
+              <p>1回</p>
+            </td>
+            <td class="contents-box_menu__inner_box_price">
+              <p>¥300,000</p>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <table class="contents-box_menu">
+        <tbody class="contents-box_menu__inner">
+          <tr class="contents-box_menu__inner_box">
+            <td class="contents-box_menu__inner_box_name width" rowspan="4">
+              <div class="flex">
+                <div class="flex_box">
+                  <p class="orange_tab">オープニング価格</p>
+                  <p>脂肪幹細胞治療（ASC）</p>
+                </div>
+                <p class="detail">肩・手・肘・股関節・膝・足関節・その他ブロック</p>
+              </div>
+            </td>
+            <td class="contents-box_menu__inner_box_part" rowspan="2">
+              <p>1関節 3V(1,500万cell）</p>
+            </td>
+            <td class="contents-box_menu__inner_box_minute">
+              <p>1回</p>
+            </td>
+            <td class="contents-box_menu__inner_box_price">
+              <p>¥1,000,000</p>
+            </td>
+          </tr>
+          <tr class="contents-box_menu__inner_box">
+            <td class="contents-box_menu__inner_box_minute">
+              <p>2回</p>
+            </td>
+            <td class="contents-box_menu__inner_box_price">
+              <p>¥1,400,000</p>
+            </td>
+          </tr>
+          <tr class="contents-box_menu__inner_box">
+            <td class="contents-box_menu__inner_box_part" rowspan="2">
+              <p>2関節 6V(3,000万cell）</p>
+            </td>
+            <td class="contents-box_menu__inner_box_minute">
+              <p>1回</p>
+            </td>
+            <td class="contents-box_menu__inner_box_price">
+              <p>¥1,400,000</p>
+            </td>
+          </tr>
+          <tr class="contents-box_menu__inner_box">
+            <td class="contents-box_menu__inner_box_minute">
+              <p>2回</p>
+            </td>
+            <td class="contents-box_menu__inner_box_price">
+              <p>¥2,000,000</p>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <table class="contents-box_menu">
+        <tbody class="contents-box_menu__inner">
+          <tr class="contents-box_menu__inner_box">
+            <td class="contents-box_menu__inner_box_name width" rowspan="4">
+              <div class="flex">
+                <div class="flex_box">
+                  <p class="orange_tab">オープニング価格</p>
+                  <p>培養上清液治療（関節内）</p>
+                </div>
+                <p class="detail">肩・手・肘・股関節・膝・足関節・その他ブロック</p>
+              </div>
+            </td>
+            <td class="contents-box_menu__inner_box_part" rowspan="2">
+              <p>1関節　5ml　</p>
+            </td>
+            <td class="contents-box_menu__inner_box_minute">
+              <p>1回</p>
+            </td>
+            <td class="contents-box_menu__inner_box_price">
+              <p>¥60,000</p>
+            </td>
+          </tr>
+          <tr class="contents-box_menu__inner_box">
+            <td class="contents-box_menu__inner_box_minute">
+              <p>3回セット</p>
+            </td>
+            <td class="contents-box_menu__inner_box_price">
+              <p>¥150,000</p>
+            </td>
+          </tr>
+          <tr class="contents-box_menu__inner_box">
+            <td class="contents-box_menu__inner_box_part" rowspan="2">
+              <p>22関節 10ml (5ml×2)</p>
+            </td>
+            <td class="contents-box_menu__inner_box_minute">
+              <p>1回</p>
+            </td>
+            <td class="contents-box_menu__inner_box_price">
+              <p>¥100,000</p>
+            </td>
+          </tr>
+          <tr class="contents-box_menu__inner_box">
+            <td class="contents-box_menu__inner_box_minute">
+              <p>3回セット</p>
+            </td>
+            <td class="contents-box_menu__inner_box_price">
+              <p>¥280,000</p>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <table class="contents-box_menu">
+        <tbody class="contents-box_menu__inner">
+          <tr class="contents-box_menu__inner_box">
+            <td class="contents-box_menu__inner_box_name width" rowspan="4">
+              <div class="flex">
+                <div class="flex_box">
+                  <p class="orange_tab">オープニング価格</p>
+                  <p>ハイドロリリース</p>
+                </div>
+              </div>
+            </td>
+            <td class="contents-box_menu__inner_box_part" rowspan="4">
+              <p>培養上清液1ml～5ml</p>
+            </td>
+            <td class="contents-box_menu__inner_box_minute">
+              <p>1ml</p>
+            </td>
+            <td class="contents-box_menu__inner_box_price">
+              <p>¥15,000</p>
+            </td>
+          </tr>
+          <tr class="contents-box_menu__inner_box">
+            <td class="contents-box_menu__inner_box_minute">
+              <p>3ml</p>
+            </td>
+            <td class="contents-box_menu__inner_box_price">
+              <p>¥40,000</p>
+            </td>
+          </tr>
+          <tr class="contents-box_menu__inner_box">
+            <td class="contents-box_menu__inner_box_minute">
+              <p>5ml</p>
+            </td>
+            <td class="contents-box_menu__inner_box_price">
+              <p>¥60,000</p>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <table class="contents-box_menu">
+        <tbody class="contents-box_menu__inner">
+          <tr class="contents-box_menu__inner_box">
+            <td class="contents-box_menu__inner_box_name width" rowspan="2">
+              <div class="flex">
+                <div class="flex_box">
+                  <p class="orange_tab">オープニング価格</p>
+                  <p>体外衝撃波（収束型）＋ 培養上清液治療</p>
+                </div>
+              </div>
+            </td>
+            <td class="contents-box_menu__inner_box_part" rowspan="1">
+              <p>-</p>
+            </td>
+            <td class="contents-box_menu__inner_box_minute">
+              <p>1回</p>
+            </td>
+            <td class="contents-box_menu__inner_box_price">
+              <p>※上清液＋衝撃波1回から、− ¥5,000</p>
+            </td>
+          </tr>
+          <tr class="contents-box_menu__inner_box">
+            <td class="contents-box_menu__inner_box_part" rowspan="1">
+              <p>-</p>
+            </td>
+            <td class="contents-box_menu__inner_box_minute">
+              <p>3回セット</p>
+            </td>
+            <td class="contents-box_menu__inner_box_price">
+              <p>※上清液＋衝撃波3回から、− ¥15,000</p>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <div class="contents-box_caution">
+        <p>※オープニング価格は年内まで。</p>
+      </div>
+      <a href="" class="contents-box_btn">
+        <img src="<?php echo $rootDir ?>/images/price/price_btn.png" alt="">
+        <p>再生医療についてはこちら</p>
+      </a>
+    </div>
+
+    <div class="contents-box">
+      <div class="contents-box_cat">
+        <p>運動療法</p>
+      </div>
+      <table class="contents-box_menu">
+        <tbody class="contents-box_menu__inner">
+          <tr class="contents-box_menu__inner_box">
+            <td class="contents-box_menu__inner_box_name">
+              <div class="flex">
+                <div class="flex_box">
+                  <p>DDシステム</p>
+                </div>
+              </div>
+            </td>
+            <td class="contents-box_menu__inner_box_exp"></td>
+            <td class="contents-box_menu__inner_box_price width" rowspan="4"><p>※セットでのご提案がベースとなります</p></td>
+          </tr>
+          <tr class="contents-box_menu__inner_box">
+            <td class="contents-box_menu__inner_box_name">
+              <div class="flex">
+                <div class="flex_box">
+                  <p>D-Wall</p>
+                </div>
+              </div>
+            </td>
+            <td class="contents-box_menu__inner_box_exp"></td>
+          </tr>
+          <tr class="contents-box_menu__inner_box">
+            <td class="contents-box_menu__inner_box_name" rowspan="2">
+              <div class="flex">
+                <div class="flex_box">
+                  <p>MTXアカデミー</p>
+                </div>
+              </div>
+            </td>
+            <td class="contents-box_menu__inner_box_exp"><p>トレーナーによる強化・トレーニング</p></td>
+          </tr>
+          <tr class="contents-box_menu__inner_box">
+            <td class="contents-box_menu__inner_box_exp"><p>HAL</p></td>
+          </tr>
+        </tbody>
+      </table>
+      <div class="contents-box_caution">
+        <p>　</p>
+      </div>
+      <a href="" class="contents-box_btn">
+        <img src="<?php echo $rootDir ?>/images/price/price_btn.png" alt="">
+        <p>運動療法についてはこちら</p>
+      </a>
     </div>
 
   </div>
