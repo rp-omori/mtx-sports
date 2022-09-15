bd146e0cf5773ac7051bccadc43fc1fefe7b509e
小池　建 <t.koike@roseaupensant.jp>
2022-09-15 21:02:35 +0900

    fix: 絞り込み検索修正

--- archive-column.php
+++ archive-column.php
@@ -168,172 +168,25 @@ $rootDir = get_template_directory_uri();
           </ul>
         </div>
 
-<script>
-/* 初期段階の表示の制御 */
-{
 
-  window.addEventListener('load', function() {
-      init()
-      linkCreate();
-  })
-
-  /* JSONの形として排出 */
-  const getWPData = () => {
-      const data = JSON.parse('<?php echo $WP_post_json; ?>');
-      // console.log(data);
-      return data;
-  }
-
-  const init = () => {
-    const containerArea = document.querySelector('.column__list__deploy')
-    let dataArr = getWPData()
-    let filterArticleArr = [] // "事例"絞り込み用
-    let CheckboxArr = [] // チェックボックスでの絞り込み用
-    let displayFlag = false
-
-    /* ページロード時の初期表示作成 */
-    const createArticleList = (type) => {
-      let articleList = ''
-
-      updateListArr().forEach(item => {
-        articleList += `
-          <li class="column__item">
-            <a href="${item.url}" class="column__link">
-              <div class="column__head">
-                <img src="${item.thumbnail}" alt="">
-              </div>
-              <div class="column__body">
-                <h3 class="column__title">${item.title}</h3>
-                <p class="column__date">${item.date}</p>
-                  <div class="column__category-wrapper">
-          `
-        if (item.column_term && item.column_term.length !== 0) {
-            item.column_term.forEach(column_termItem => {
-                articleList += `
-                  <span class="column__category">${column_termItem}</span>
-                                        `
-            })
-        }
-
-          articleList += `
-              </div>
-            </a>
-          </li>
-              `
-      })
-
-      containerArea.innerHTML = articleList
-      // console.log(articleList);
-
-      // console.log(articleList);
-
-      // if (type === 'insert') {
-      //     containerArea.insertAdjacentHTML('afterbegin', articleList)
-      // } else {
-      //     containerArea.innerHTML = articleList
-      // }
-    }
-
-      /* 絞り込みボタンを押したときの挙動 */
-      const doCheckItem = () => {
-          const columnCheckList = document.querySelectorAll('input[name="columns"]')
-          const checkboxInput = document.querySelectorAll('.column__tags-check')
-        console.log(columnCheckList);
-          //タグの絞り込み
-          columnCheckList.forEach(checkItem => {
-              checkItem.addEventListener("change", () => {
-                  if (checkItem.checked) {
-                      CheckboxArr.push(checkItem.value)
-                  } else {
-                      CheckboxArr = CheckboxArr.filter(item => {
-                          return item !== checkItem.value
-                      })
-                  }
-
-                  // filterItems();
-                  // linkCreate();
-              })
-          })
-
-          // 絞り込みボタン押下時
-          const tagSearchButton = document.getElementById('tag_search')
-          // console.log(tagSearchButton);
-          tagSearchButton.addEventListener('click', () => {
-            // filterItems();
-            linkCreate();
-            console.log('aaa');
-          })
-
-      }
-
-
-      // const filterItems = () => {
-      //     filterArticleArr = dataArr.filter(data => {
-      //         return CheckboxArr.reduce((prev, current) => {
-      //             return prev || data.column.includes(current)
-      //         }, false)
-      //     })
-      //     createArticleList();
-      //     linkCreate();
-      // }
-
-
-      /*
-        * @summary 絞り込み検索で生成した配列が空だったら全件配列、そうでなければ絞り込み検索で生成した配列を返す
-        * @return データ一覧の配列を返す
-        */
-      const updateListArr = () => {
-          if (filterArticleArr.length === 0) {
-              if (CheckboxArr.length === 0) {
-                  return dataArr
-              } else if (CheckboxArr.length >= 1 && filterArticleArr.length === 0) {
-                  return []
-              }
-          } else {
-              return filterArticleArr
-          }
-      }
-
-      linkCreate();
-      createArticleList();
-      doCheckItem();
-
-  }
-  // リンク生成
-  const linkCreate = () => {
-      const containerArea = document.querySelectorAll('.column__item')
-      containerArea.forEach(item => {
-          item.addEventListener("click", () => {
-              location.href = item.dataset.url;
-          });
-      })
-  }
-
-
-
-}
-
-
-
-</script>
 
       </div>
       <div class="pagination">
-      <?php
-        global $wp_query;
-        $big = 999999999; // need an unlikely integer
+        <?php
+          global $wp_query;
+          $big = 999999999; // need an unlikely integer
 
-        echo paginate_links( array(
-          'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
-          'format' => '?paged=%#%',
-          'current' => max( 1, get_query_var('paged') ),
-          'total' => $wp_query->max_num_pages,
-          'type' => 'list',
-          'mid_size' => '1',
-          'prev_text' => '',
-          'next_text' => '',
-        ) );
-      ?>
+          echo paginate_links( array(
+            'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+            'format' => '?paged=%#%',
+            'current' => max( 1, get_query_var('paged') ),
+            'total' => $wp_query->max_num_pages,
+            'type' => 'list',
+            'mid_size' => '1',
+            'prev_text' => '',
+            'next_text' => '',
+          ) );
+        ?>
       </div>
     </div>
     <?php get_template_part('component/footer__other'); ?>
@@ -341,3 +194,144 @@ $rootDir = get_template_directory_uri();
 </main>
 
 <?php get_footer(); ?>
+
+
+<script>
+      /* 初期段階の表示の制御 */
+      {
+
+        window.addEventListener('load', function() {
+            init()
+            linkCreate();
+        })
+
+        /* JSONの形として排出 */
+        const getWPData = () => {
+            const data = JSON.parse('<?php echo $WP_post_json; ?>');
+            console.log(data);
+            return data;
+        }
+
+        const init = () => {
+          const containerArea = document.querySelector('.column__list__deploy')
+          let dataArr = getWPData()
+          let filterArticleArr = [] // "事例"絞り込み用
+          let CheckboxArr = [] // チェックボックスでの絞り込み用
+          let displayFlag = false
+
+          /* ページロード時の初期表示作成 */
+          const createArticleList = (type) => {
+            let articleList = ''
+
+            updateListArr().forEach(item => {
+              articleList += `
+                <li class="column__item">
+                  <a href="${item.url}" class="column__link">
+                    <div class="column__head">
+                      <img src="${item.thumbnail}" alt="">
+                    </div>
+                    <div class="column__body">
+                      <h3 class="column__title">${item.title}</h3>
+                      <p class="column__date">${item.date}</p>
+                        <div class="column__category-wrapper">
+                `
+              if (item.column_term && item.column_term.length !== 0) {
+                  item.column_term.forEach(column_termItem => {
+                      articleList += `
+                        <span class="column__category">${column_termItem}</span>
+                                              `
+                  })
+              }
+
+                articleList += `
+                    </div>
+                  </a>
+                </li>
+                    `
+            })
+
+            containerArea.innerHTML = articleList
+
+            // if (type === 'insert') {
+            //     containerArea.insertAdjacentHTML('afterbegin', articleList)
+            // } else {
+            //     containerArea.innerHTML = articleList
+            // }
+
+          }
+
+            /* 絞り込みボタンを押したときの挙動 */
+            const doCheckItem = () => {
+                const checkboxInput = document.querySelectorAll('input[name="columns"]')
+                const columnCheckList = document.querySelectorAll('.column__tags-label')
+                //タグの絞り込み
+                checkboxInput.forEach(checkItem => {
+                    checkItem.addEventListener("change", () => {
+                        if (checkItem.checked) {
+                            CheckboxArr.push(checkItem.value)
+                            console.log(CheckboxArr)
+                        } else {
+                            CheckboxArr = CheckboxArr.filter(item => {
+                              console.log(CheckboxArr)
+                                return item !== checkItem.value
+                            })
+                        }
+                    })
+                })
+
+                // 絞り込みボタン押下時
+                const tagSearchButton = document.getElementById('tag_search')
+                tagSearchButton.addEventListener('click', () => {
+                  filterItems();
+                  linkCreate();
+                })
+
+            }
+
+
+            const filterItems = () => {
+              filterArticleArr = dataArr.filter(data => {return CheckboxArr.reduce((prev, current) => {return prev || data.column_tag.includes(current)}, false)})
+              createArticleList();
+              linkCreate();
+            }
+
+
+            /*
+              * @summary 絞り込み検索で生成した配列が空だったら全件配列、そうでなければ絞り込み検索で生成した配列を返す
+              * @return データ一覧の配列を返す
+              */
+            const updateListArr = () => {
+                if (filterArticleArr.length === 0) {
+                    if (CheckboxArr.length === 0) {
+                        return dataArr
+                    } else if (CheckboxArr.length >= 1 && filterArticleArr.length === 0) {
+                        return []
+                    }
+                } else {
+                    return filterArticleArr
+                }
+            }
+
+            linkCreate();
+            createArticleList();
+            doCheckItem();
+
+        }
+
+        // リンク生成
+        const linkCreate = () => {
+            const containerArea = document.querySelectorAll('.column__item')
+            containerArea.forEach(item => {
+                item.addEventListener("click", () => {
+                    location.href = item.dataset.url;
+                });
+            })
+        }
+
+
+
+      }
+
+
+
+</script>
